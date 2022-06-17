8b5a1610d28498f9fac9a5d046fc46182d76a59f
Abbas Doodabi <a.doodabi7@gmail.com>
2022-06-17 12:07:19 +0430

    Improve enroll method breakdown

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -6,47 +6,58 @@ import domain.exceptions.EnrollmentRulesViolationException;
 
 public class EnrollCtrl {
 	public void enroll(Student student, List<CSE> courses) throws EnrollmentRulesViolationException {
-		for (CSE course_sescion : courses) {
-			if (student.hasPassed(course_sescion.getCourse()))
-					throw new EnrollmentRulesViolationException(String.format("The student has already passed %s",
-							course_sescion.getCourse().getName()));
-			checkPrerequisites(student, course_sescion);
-			checkExamTimeConflict(course_sescion, courses);
-			checkDuplicateRequest(course_sescion, courses);
-		}
+		checkForAlreadyPassedCourses(student, courses);
+		checkPrerequisites(student, courses);
+		checkExamTimeConflict(courses);
+		checkDuplicateRequest(courses);
 		checkRequestedUnitsLimit(student, courses);
+
 		for (CSE course_section : courses)
 			student.takeCourse(course_section.getCourse(), course_section.getSection());
 	}
 
-	private void checkPrerequisites(Student student, CSE course_section) throws EnrollmentRulesViolationException
+	private void checkForAlreadyPassedCourses(Student student, List<CSE> courses) throws EnrollmentRulesViolationException
 	{
-		final List<Course> prereqs = course_section.getCourse().getPrerequisites();
-		for (Course pre : prereqs)
-			if (!student.hasPassed(pre))
-				throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a"
-						+ " prerequisite of %s", pre.getName(), course_section.getCourse().getName()));
+		for (CSE course_section : courses)
+			if (student.hasPassed(course_section.getCourse()))
+					throw new EnrollmentRulesViolationException(String.format("The student has already passed %s",
+							course_section.getCourse().getName()));
 	}
 
-	private void checkExamTimeConflict(CSE course_section, List<CSE> courses) throws EnrollmentRulesViolationException
+	private void checkPrerequisites(Student student, List<CSE> courses) throws EnrollmentRulesViolationException
 	{
-		for (CSE cse : courses) {
-			if (course_section == cse)
-				continue;
-			if (course_section.getExamTime().equals(cse.getExamTime()))
-				throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam"
-						+ " time", course_section, cse));
+		for (CSE course_section : courses) {
+			final List<Course> prereqs = course_section.getCourse().getPrerequisites();
+			for (Course pre : prereqs)
+				if (!student.hasPassed(pre))
+					throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a"
+							+ " prerequisite of %s", pre.getName(), course_section.getCourse().getName()));
 		}
 	}
 
-	private void checkDuplicateRequest(CSE course_section, List<CSE> courses) throws EnrollmentRulesViolationException
+	private void checkExamTimeConflict(List<CSE> courses) throws EnrollmentRulesViolationException
 	{
 		for (CSE cse : courses) {
-			if (course_section == cse)
-				continue;
-			if (course_section.getCourse().equals(cse.getCourse()))
-				throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice",
-						course_section.getCourse().getName()));
+			for (CSE other : courses) {
+				if (cse == other)
+					continue;
+				if (cse.getExamTime().equals(other.getExamTime()))
+					throw new EnrollmentRulesViolationException(String.format("Two offerings %s and %s have the same exam"
+							+ " time", cse, other));
+			}
+		}
+	}
+
+	private void checkDuplicateRequest(List<CSE> courses) throws EnrollmentRulesViolationException
+	{
+		for (CSE cse : courses) {
+			for (CSE other : courses) {
+				if (cse == other)
+					continue;
+				if (cse.getCourse().equals(other.getCourse()))
+					throw new EnrollmentRulesViolationException(String.format("%s is requested to be taken twice",
+							cse.getCourse().getName()));
+			}
 		}
 	}
 
